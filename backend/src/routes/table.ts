fee3d17ef4b34965afa7ad83eee35a4ff6ce7fd3
printer83mph <printer.83mph@gmail.com>
2022-04-21 00:28:48 -0400

    Improve backup code

--- backend/src/routes/table.ts
+++ backend/src/routes/table.ts
@@ -9,26 +9,39 @@ const TICK = 150
 const TableRouter = express.Router()
 const listeners = []
 
-let antSim
-
+// find latest backup
 Backup.findOne({}, {}, { sort: { date: -1 } }, (err, data) => {
   if (err) {
-    throw err
+    console.log('Unable to load backup!')
   }
+
   console.log(`Loaded backup from ${data.date.toLocaleString()}`)
-  antSim = new AntSim(JSON.parse(data.backup))
+  const antSim = new AntSim(JSON.parse(data.backup))
+  // const antSim = new AntSim()
 
   const backupSim = async () => {
+    const currentTime = new Date()
     const backup = JSON.stringify(antSim.toExport())
-    const backupDoc = new Backup({ date: new Date(), backup })
+    const backupDoc = new Backup({ date: currentTime, backup })
     await backupDoc.save()
     console.log('Saved backup!')
+
+    // delete old backups
+    const tenDaysBack = new Date()
+    tenDaysBack.setDate(currentTime.getDate() - 10)
+    const oldBackups = Backup.find({ date: { $lte: tenDaysBack } })
+    if ((await oldBackups.count()) !== 0) {
+      await oldBackups.deleteMany({})
+      console.log('Deleted old backups.')
+    }
     setTimeout(backupSim, oneDay * 0.5)
     // TODO: delete old backups
   }
 
-  // wait 30 minutes then backup
-  setTimeout(backupSim, 1000 * 60 * 30)
+  // wait 15 minutes then backup
+  setTimeout(backupSim, 1000 * 60 * 15)
+
+  process.on('exit', backupSim)
 
   setInterval(() => {
     antSim.update(TICK)
@@ -40,20 +53,20 @@ Backup.findOne({}, {}, { sort: { date: -1 } }, (err, data) => {
   }, TICK)
 
   TableRouter.ws('/', (ws) => {
-    console.log('table connection opened!')
+    // console.log('table connection opened!')
 
     const listener = (data) => ws.send(data)
     listeners.push(listener)
 
     ws.on('close', () => {
-      console.log('table connection closed!')
+      // console.log('table connection closed!')
       listeners.splice(listeners.indexOf(listener), 1)
     })
   })
-})
 
-TableRouter.get('/dead', (req, res, next) => {
-  res.status(200).json({ deadAnts: antSim.state.deadAnts })
+  TableRouter.get('/dead', (req, res, next) => {
+    res.status(200).json({ deadAnts: antSim.state.deadAnts })
+  })
 })
 
 export default TableRouter
